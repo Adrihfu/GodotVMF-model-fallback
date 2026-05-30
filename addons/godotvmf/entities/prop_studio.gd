9b909560f3a5d1e2643a68030927b1eca8a5af5f
Adrián García <adrihfu@hotmail.com>
2026-05-31 01:39:01 +0200

    Updated prop_studio.gd
    Added model fallback for:
    - ´PackedScene´ 's with a MeshInstance3D scene root _(mdl and tscn/scn)_
    - ´Mesh´ resources _(obj, mesh, tres, res)_
    - ´PackedScene´ 's without a MeshInstance3D as scene root _(glb, gltf, fbx)_

--- addons/godotvmf/entities/prop_studio.gd
+++ addons/godotvmf/entities/prop_studio.gd
@@ -28,9 +28,20 @@ func _entity_setup(_entity: VMFEntity) -> void:
 
 			VMFLogger.warn("Model not found: " + model_path);
 			VMFCache.add_logged_file(model);
-			return;
 
-		model_scene = ResourceLoader.load(model_path) as PackedScene;
+			model_path = VMFConfig.models.fallback_model;
+
+			if not model_path or not ResourceLoader.exists(model_path): return;
+
+			var model_resource: = ResourceLoader.load(model_path);
+			if model_resource.is_class("Mesh"):
+				model_scene = PackedScene.new();
+				var mesh_instance := MeshInstance3D.new();
+				mesh_instance.mesh = model_resource;
+				mesh_instance.name = "fallback_model";
+				model_scene.pack(mesh_instance);
+
+		model_scene = ResourceLoader.load(model_path) as PackedScene if not model_scene else model_scene;
 		VMFCache.add_cached(model, model_scene);
 
 		if not model_scene:
@@ -40,7 +51,15 @@ func _entity_setup(_entity: VMFEntity) -> void:
 	var instance := model_scene.instantiate(PackedScene.GEN_EDIT_STATE_MAIN_INHERITED);
 	instance.name = "model";
 	instance.scale *= model_scale;
-
+	
+	if not instance is MeshInstance3D:
+		self.add_child(instance);
+		var mesh_parent: = MeshInstance3D.new();
+		mesh_parent.name = instance.name;
+		instance.reparent(mesh_parent, false);
+		instance.owner = mesh_parent;
+		instance = mesh_parent;
+	
 	MDLCombiner.apply_skin(instance, skin);
 
 	add_child(instance);
